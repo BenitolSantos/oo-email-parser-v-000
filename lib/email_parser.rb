a822630b5cccab4759e28c5a5084f427c0468a15
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-02 21:36:50 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,6 @@ class EmailParser
   attr_accessor :emails #the only attribute is a list of emails as a string.
 
   def parse #parse doesn't expect any arguments.
-    binding.pry
     self.emails.split(/,\s/) #regex of , and any s whitespace character
   end
 end
